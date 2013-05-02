5ed70e77a14766a4cb7c2b633a88b6e7ccc1e0cc
snthd <158rune@gmail.com>
2013-05-02 18:29:58 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uchitter::Application.config.secret_key_base = '55903598595328772be144fbf664a78d5e3a8c71d8fe543151bd6dc8ffebc178c6d186c01777986fcd5a58c263fe17fc32ca890ce880d2381a9add9a43ad081e'
